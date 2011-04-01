49cce0eb80ac3caa19c56e13d638aecb5108c25a
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-01 16:49:01 +0200

    Created App skeleton

---
+++ App.cs
@@ -0,0 +1,17 @@
+﻿// Arnold Overwater - 0821508 - INF2A
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class App {
+
+	private static int Main(string[] args) {
+		// Make sure the output window stays open until a key is hit
+		Console.WriteLine("The end (press any key to exit)");
+		Console.ReadKey();
+		return 0; // Application succesful
+	}
+
+}
