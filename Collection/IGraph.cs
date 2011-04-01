6af865fa37cce13ce2ff890e2262d1ff2d5d4a56
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-01 17:10:37 +0200

    Added Graph interface

---
+++ Collection/IGraph.cs
@@ -0,0 +1,18 @@
+﻿// Arnold Overwater - 0821508 - INF2A
+
+using System;
+using System.Collections.Generic;
+
+namespace Collection.Graph {
+
+	interface IGraph<E, V> : IList<E> {
+
+		public V GetVertex(int from, int to);
+
+		public V[] GetVerticesFrom(int index);
+
+		public V[] GetVerticesTo(int index);
+
+	}
+
+}
