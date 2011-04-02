cde2ea2596928301e212d9399d3d97531b11ac40
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-02 11:38:06 +0200

    Created skeleton of UndirectionalGraph class

---
+++ Collection/UndirectionalGraph.cs
@@ -0,0 +1,20 @@
+﻿// Arnold Overwater - 0821508 - INF2A
+
+using System;
+using System.Collections.Generic;
+
+namespace Collection.Graph {
+
+	class UndirectionalGraph<E, V> : Graph<E, V> {
+
+		public override V GetVertex(int from, int to) {
+			throw new NotImplementedException();
+		}
+
+		public override void RemoveAt(int index) {
+			throw new NotImplementedException();
+		}
+
+	}
+
+}
