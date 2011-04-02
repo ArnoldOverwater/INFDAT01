e794a2b1087e8e3e120bf9cb36c1b06f233d8ca6
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-02 12:25:12 +0200

    Implemented methods in UndirectionalGraph class

--- Collection/UndirectionalGraph.cs
+++ Collection/UndirectionalGraph.cs
@@ -2,17 +2,39 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Collection.Graph {
 
 	class UndirectionalGraph<E, V> : Graph<E, V> {
 
 		public override V GetVertex(int from, int to) {
-			throw new NotImplementedException();
+			if (from < to) {
+				int temp = to;
+				to = from;
+				from = temp;
+			}
+			Dictionary<int, V> dictionary = edges[from].vertices;
+			if (dictionary.ContainsKey(to))
+				return dictionary[to];
+			else
+				return DefaultValue;
 		}
 
 		public override void RemoveAt(int index) {
-			throw new NotImplementedException();
+			edges.RemoveAt(index);
+			for (int i = index; i < edges.Count; i++)
+				edges[i].vertices.Remove(index);
+			foreach (Edge edge in edges) {
+				Dictionary<int, V> dictionary = edge.vertices;
+				var indices = from i in dictionary.Keys
+					where i > index
+					select new {oldIndex = i, newIndex = i - 1, element = dictionary[i]};
+				foreach (var v in indices) {
+					dictionary.Remove(v.oldIndex);
+					dictionary.Add(v.newIndex, v.element);
+				}
+			}
 		}
 
 	}
