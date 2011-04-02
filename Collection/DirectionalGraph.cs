cf40a1caf1dff02991bb28d181850d3dc795cf58
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-02 12:33:22 +0200

    Implemented methods in DirectionalGraph

--- Collection/DirectionalGraph.cs
+++ Collection/DirectionalGraph.cs
@@ -2,17 +2,33 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Collection.Graph {
 
 	class DirectionalGraph<E, V> : Graph<E, V> {
 
 		public override V GetVertex(int from, int to) {
-			throw new NotImplementedException();
+			Dictionary<int, V> dictionary = edges[from].vertices;
+			if (dictionary.ContainsKey(to))
+				return dictionary[to];
+			else
+				return DefaultValue;
 		}
 
 		public override void RemoveAt(int index) {
-			throw new NotImplementedException();
+			edges.RemoveAt(index);
+			foreach (Edge edge in edges) {
+				edge.vertices.Remove(index);
+				Dictionary<int, V> dictionary = edge.vertices;
+				var indices = from i in dictionary.Keys
+					where i > index
+					select new {oldIndex = i, newIndex = i - 1, element = dictionary[i]};
+				foreach (var v in indices) {
+					dictionary.Remove(v.oldIndex);
+					dictionary.Add(v.newIndex, v.element);
+				}
+			}
 		}
 
 	}
