306163db966423153db1b93e18882af2023437d1
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-10 16:06:08 +0200

    Added skeleton of Player class

---
+++ Game/Player.cs
@@ -0,0 +1,49 @@
+﻿// Arnold Overwater - 0821508 - INF2A
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Counterstrike {
+
+	class Player {
+
+		#region fields
+
+		public string ScreenName;
+
+		public IPAddress IPAddress;
+
+		private ulong totalKills;
+
+		private ulong totalKilled;
+
+		private double score;
+
+		#endregion
+
+		#region properties
+
+		public ulong TotalKills {
+			get {
+				return totalKills;
+			}
+		}
+
+		public ulong TotalKilled {
+			get {
+				return totalKilled;
+			}
+		}
+
+		public double Score {
+			get {
+				return score;
+			}
+		}
+
+		#endregion
+
+	}
+
+}
