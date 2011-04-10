3f7560b604b416f2d5a6908a318b4b1f370d13ce
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-10 20:32:51 +0200

    Implemented methods in Game class

--- Game/Game.cs
+++ Game/Game.cs
@@ -1,5 +1,7 @@
 ﻿// Arnold Overwater - 0821508 - INF2A
 
+// Arnold Overwater - 0821508 - INF2A
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,45 @@ using Collection.Graph;
 namespace Counterstrike {
 
 	public class Game : DirectionalGraph<Player, ushort> {
+
+		#region constructor
+
+		public Game() : base(0) {}
+
+		#endregion
+
+		#region methods
+
+		public void AddPlayer(Player player) {
+			Add(player);
+			player.IncrementMatches();
+		}
+
+		#endregion
+
+		#region kill methods
+
+		public void KillPlayerIndex(int killer, int victim) {
+			ushort kills = GetVertex(killer, victim);
+			kills++;
+			SetVertex(killer, victim, kills);
+			this[killer].IncrementKills();
+			this[victim].IncrementKilled();
+		}
+
+		public void KillPlayerIndex(int suicider) {
+			KillPlayerIndex(killer: suicider, victim: suicider);
+		}
+
+		public void KillPlayer(Player killer, Player victim) {
+			KillPlayerIndex(IndexOf(killer), IndexOf(victim));
+		}
+
+		public void KillPlayer(Player suicider) {
+			KillPlayerIndex(IndexOf(suicider));
+		}
+
+		#endregion
 
 	}
 
