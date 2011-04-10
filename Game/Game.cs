a1a3686e9af03ffa450a1f3f0c9c571c499b622c
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-10 16:18:55 +0200

    Created skeleton of Game class

---
+++ Game/Game.cs
@@ -0,0 +1,14 @@
+﻿// Arnold Overwater - 0821508 - INF2A
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Collection.Graph;
+
+namespace Counterstrike {
+
+	class Game : DirectionalGraph<Player, ushort> {
+
+	}
+
+}
