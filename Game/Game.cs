0cd3fe922795e436e95107e57e1ae3d617e54912
ArnoldOverwater <arnoldoverwater@hotmail.com>
2011-04-16 20:32:27 +0200

    Bug fix

--- Game/Game.cs
+++ Game/Game.cs
@@ -55,8 +55,8 @@ namespace Counterstrike {
 			SetVertex(killer, victim, kills);
 			this[killer].IncrementKills();
 			this[victim].IncrementKilled();
-			this[victim].MatchScore -= this[victim].MatchScore * Count;
-			this[killer].MatchScore += this[victim].MatchScore * Count;
+			this[victim].MatchScore -= this[victim].MatchScore / Count;
+			this[killer].MatchScore += this[victim].MatchScore / Count;
 		}
 
 		public void KillPlayerIndex(int suicider) {
